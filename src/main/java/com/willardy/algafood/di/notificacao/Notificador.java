7b9c3269ee463cc2681d9d84d27887c0b9d28839
Willardy Tyrone <willardy@gmail.com>
2021-06-16 21:43:27 -0300

    limpando projeto para modulo de JPA/HIBERNATE

--- src/main/java/com/willardy/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.willardy.algafood.di.notificacao;
-
-import com.willardy.algafood.di.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
