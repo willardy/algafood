4fa2f8b14b2daa61a2c9cd57b33c92b69a795717
Willardy Tyrone <willardy@gmail.com>
2021-06-17 21:31:22 -0300

    - Consultando as cozinhas com JPA. - Incluido para mostrar o SQL gerado pelo JPA no console.

---
+++ src/main/java/com/willardy/algafood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,22 @@
+package com.willardy.algafood.jpa;
+
+import com.willardy.algafood.ApiAlgafoodApplication;
+import com.willardy.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(ApiAlgafoodApplication.class).web(WebApplicationType.NONE).run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into cozinha (nome) values ('Tailandesa')
 insert into cozinha (nome) values ('Indiana')
+insert into cozinha (nome) values ('Brasileira')
+insert into cozinha (nome) values ('Japonesa')
+insert into cozinha (nome) values ('Portuguesa')
+insert into cozinha (nome) values ('Italiana')
