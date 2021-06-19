941747b94d6b98b06a39fe01f3f120ed2444dcac
Willardy Tyrone <willardy@gmail.com>
2021-06-19 19:51:23 -0300

    - Inserindo novas cozinhas utilizando metodo http POST e anotação @PostMapping

--- src/main/java/com/willardy/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/willardy/algafood/api/controller/CozinhaController.java
@@ -5,10 +5,7 @@ import com.willardy.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +32,11 @@ public class CozinhaController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha save(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.save(cozinha);
     }
 }
