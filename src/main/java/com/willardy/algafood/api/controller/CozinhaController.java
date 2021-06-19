547f6bf946be6ca0c3afb2bd019977787a7d7612
Willardy Tyrone <willardy@gmail.com>
2021-06-19 18:03:51 -0300

    - Implementando a busca por uma cozinha baseado no Id (Singleton Resource) - Incluido no POM.XML a dependencia para gerar em formato XML

--- src/main/java/com/willardy/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/willardy/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.willardy.algafood.domain.model.Cozinha;
 import com.willardy.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> all() {
         return cozinhaRepository.all();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha findById(@PathVariable Long id){
+        return cozinhaRepository.findById(id);
     }
 }
