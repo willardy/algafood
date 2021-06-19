bd6c7201070ba0639e606f11994ba41df0399ff4
Willardy Tyrone <willardy@gmail.com>
2021-06-19 17:30:43 -0300

    -Criando primeira Resource Colletion - Cozinhas

---
+++ src/main/java/com/willardy/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.willardy.algafood.api.controller;
+
+import com.willardy.algafood.domain.model.Cozinha;
+import com.willardy.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> all() {
+        return cozinhaRepository.all();
+    }
+}
