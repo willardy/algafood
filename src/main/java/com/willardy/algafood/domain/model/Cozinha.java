4c913e7165b97db263d4a2452a46e10795f4957f
Willardy Tyrone <willardy@gmail.com>
2021-06-19 18:13:37 -0300

    - Utilizando @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/com/willardy/algafood/domain/model/Cozinha.java
+++ src/main/java/com/willardy/algafood/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.willardy.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+//@JsonRootName("cozinha") //Muda o rootname do retorno do cliente (nao visível em Json por nao mostrar o nome root)
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -16,5 +20,7 @@ public class Cozinha {
     private Long id;
 
     @Column(nullable = false)
+//    @JsonProperty("titulo") //Muda no retorno ao cliente
+//    @JsonIgnore // propriedade que ignora no retorno ao cliente
     private String nome;
 }
