713ebe1446f998352fc382fc68cec990b0239012
Willardy Tyrone <willardy@gmail.com>
2021-06-23 22:28:13 -0300

    - Implementando uma consulta simples com Criteria API

--- src/main/java/com/willardy/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/willardy/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -20,29 +22,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0=0 ");
 
-        var parametros = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        criteria.from(Restaurante.class);
 
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", '%' + nome + '%');
-        }
-
-        if (taxaInicial != null) {
-            jpql.append("and taxaFrete >= :taxaInicial ");
-            parametros.put("taxaInicial", taxaInicial);
-        }
-
-        if (taxaFinal != null){
-            jpql.append("and taxaFrete <= :taxaFinal ");
-            parametros.put("taxaFinal", taxaFinal);
-        }
-
-        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-        parametros.forEach((key, value) -> query.setParameter(key, value));
-
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
         return query.getResultList();
     }
 }
