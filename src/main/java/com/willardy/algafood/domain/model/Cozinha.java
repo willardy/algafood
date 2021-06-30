100852dea222f81f015ce2a0940bfde5ca23256b
Willardy Tyrone <willardy@gmail.com>
2021-06-29 21:56:55 -0300

    - Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/willardy/algafood/domain/model/Cozinha.java
+++ src/main/java/com/willardy/algafood/domain/model/Cozinha.java
@@ -1,12 +1,12 @@
 package com.willardy.algafood.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -20,4 +20,8 @@ public class Cozinha {
 
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha") //Mapeado pelo dono do relacionamento, neste caso Restaurante
+    private List<Restaurante> restaurantes = new ArrayList<>();
 }
