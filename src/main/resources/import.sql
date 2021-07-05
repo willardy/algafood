ffe28293cd63b45e4fb6c50dfa5693559b9561de
Willardy Tyrone <willardy@gmail.com>
2021-07-05 19:16:37 -0300

    - Incluido JsonIgnore para nao trazer dados de Endereço pelo Restaurante (Evitar impacto de multiplas consultas) - Inlcuido SQL de restaurante com todos os dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,11 +3,6 @@ INSERT INTO cozinha (nome) values ('Indiana')
 INSERT INTO cozinha (nome) values ('Portuguesa')
 INSERT INTO cozinha (nome) values ('Italiana')
 
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La pizzaria do João', 1.96, 1)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La casa del Maria', 5.96,3)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Arrumadinho da API', 0.00,4)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Frete da API', 0.00,1)
-
 INSERT INTO estado (nome) values ('Piauí')
 INSERT INTO estado (nome) values ('Maranhão')
 INSERT INTO estado (nome) values ('Ceará')
@@ -16,6 +11,12 @@ INSERT INTO cidade (nome, estado_id) values ('Teresina', 1)
 INSERT INTO cidade (nome, estado_id) values ('São Luis', 2)
 INSERT INTO cidade (nome, estado_id) values ('Fortaleza', 3)
 INSERT INTO cidade (nome, estado_id) values ('São Raimundo Nonato', 1)
+
+INSERT INTO restaurante (endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, nome, taxa_frete, cozinha_id, endereco_cidade_id) VALUES ('Ininga', '987654-480', 'Perto daquele lugar', '', '4355', 'La France del peru', 9.55, 1, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La pizzaria do João', 1.96, 1)
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La casa del Maria', 5.96,3)
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Arrumadinho da API', 0.00,4)
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Frete da API', 0.00,1)
 
 INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de Crédito')
 INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de Débito')
