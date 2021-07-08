65c25e90265aa7ed57d06c915cbb199c5acb4873
Willardy Tyrone <willardy@gmail.com>
2021-07-07 22:07:40 -0300

    - Desafio: mapeando relacionamento um-para-muitos

--- src/main/java/com/willardy/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/willardy/algafood/api/controller/RestauranteController.java
@@ -60,7 +60,7 @@ public class RestauranteController {
             Optional<Restaurante> restauranteAtual = restauranteRepository.findById(id);
 
             if(restauranteAtual.isPresent()){
-                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "dataCadastro");
+                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
                 Restaurante restauranteNovo = cadastroRestauranteService.saveOrUpdate(restauranteAtual.get());
                 return ResponseEntity.ok(restauranteNovo);
