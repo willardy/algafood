7841fe7efb51580180bb55e2ade91c17639c7d48
Willardy Tyrone <willardy@gmail.com>
2021-07-07 22:21:15 -0300

    - Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/willardy/algafood/domain/model/Grupo.java
@@ -0,0 +1,26 @@
+package com.willardy.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes;
+}
