5462037743efe52d63b66083cd659d9efb41539b
Willardy Tyrone <willardy@gmail.com>
2021-07-07 21:12:49 -0300

    - Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

--- src/main/java/com/willardy/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/willardy/algafood/api/controller/RestauranteController.java
@@ -60,7 +60,7 @@ public class RestauranteController {
             Optional<Restaurante> restauranteAtual = restauranteRepository.findById(id);
 
             if(restauranteAtual.isPresent()){
-                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento");
+                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "dataCadastro");
 
                 Restaurante restauranteNovo = cadastroRestauranteService.saveOrUpdate(restauranteAtual.get());
                 return ResponseEntity.ok(restauranteNovo);
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,11 +12,11 @@ INSERT INTO cidade (nome, estado_id) values ('São Luis', 2)
 INSERT INTO cidade (nome, estado_id) values ('Fortaleza', 3)
 INSERT INTO cidade (nome, estado_id) values ('São Raimundo Nonato', 1)
 
-INSERT INTO restaurante (endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, nome, taxa_frete, cozinha_id, endereco_cidade_id) VALUES ('Ininga', '987654-480', 'Perto daquele lugar', '', '4355', 'La France del peru', 9.55, 1, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La pizzaria do João', 1.96, 1)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('La casa del Maria', 5.96,3)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Arrumadinho da API', 0.00,4)
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Frete da API', 0.00,1)
+INSERT INTO restaurante (data_cadastro, data_atualizacao, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, nome, taxa_frete, cozinha_id, endereco_cidade_id) VALUES (utc_timestamp, utc_timestamp, 'Ininga', '987654-480', 'Perto daquele lugar', '', '4355', 'La France del peru', 9.55, 1, 1);
+INSERT INTO restaurante (data_cadastro, data_atualizacao, nome, taxa_frete, cozinha_id) values (utc_timestamp, utc_timestamp, 'La pizzaria do João', 1.96, 1)
+INSERT INTO restaurante (data_cadastro, data_atualizacao, nome, taxa_frete, cozinha_id) values (utc_timestamp, utc_timestamp, 'La casa del Maria', 5.96,3)
+INSERT INTO restaurante (data_cadastro, data_atualizacao, nome, taxa_frete, cozinha_id) values (utc_timestamp, utc_timestamp, 'Arrumadinho da API', 0.00,4)
+INSERT INTO restaurante (data_cadastro, data_atualizacao, nome, taxa_frete, cozinha_id) values (utc_timestamp, utc_timestamp, 'Frete da API', 0.00,1)
 
 INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de Crédito')
 INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de Débito')
