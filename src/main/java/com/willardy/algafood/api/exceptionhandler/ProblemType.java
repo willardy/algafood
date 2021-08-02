df4b5a363212fdddffab3e306a86e1ae432295bf
Willardy Tyrone <willardy@gmail.com>
2021-08-02 17:58:28 -0300

    - Desafio: tratando outras exceções não capturadas

--- src/main/java/com/willardy/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/willardy/algafood/api/exceptionhandler/ProblemType.java
@@ -8,6 +8,7 @@ public enum ProblemType {
     RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
     PARAMETRO_INVALIDO("/parametro-invalido", "Parámetro inválido");
 
     private String title;
