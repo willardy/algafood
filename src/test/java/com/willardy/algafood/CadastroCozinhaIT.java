c96496f3c1d1625278da02bc84ffd9612e660169
Willardy Tyrone <willardy@gmail.com>
2021-08-10 18:37:09 -0300

    - Configurando Maven Failsafe Plugin no projeto - Implementando Testes de API com REST Assured e validando o código de status HTTP

---
+++ src/test/java/com/willardy/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,35 @@
+package com.willardy.algafood;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static io.restassured.RestAssured.given;
+
+//Configuração do JUNIT 5
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CadastroCozinhaIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
