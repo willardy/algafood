645aa03fd07b8c0653d70c6527ad32d02a14b197
Willardy Tyrone <willardy@gmail.com>
2021-08-10 18:51:36 -0300

    - Criando um método para fazer setup dos testes

--- src/test/java/com/willardy/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/willardy/algafood/CadastroCozinhaIT.java
@@ -2,6 +2,7 @@ package com.willardy.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 //Configuração do JUNIT 5
 @ExtendWith(SpringExtension.class)
@@ -19,17 +22,32 @@ class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
         given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
                 .when()
                 .get()
                 .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas() {
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("", hasSize(4))
+                //Essa linha nao é necessario nesse teste, porem fica de exemplo para testes onde precisam validar campos dos objetos de retorno
+                .body("nome", hasItems("Tailandesa", "Brasileira"));
     }
 }
