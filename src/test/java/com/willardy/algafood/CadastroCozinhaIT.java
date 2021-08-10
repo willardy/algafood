78b3a0e69fb851e130b33502683ffba9290bdadb
Willardy Tyrone <willardy@gmail.com>
2021-08-10 19:02:15 -0300

    - Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/willardy/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/willardy/algafood/CadastroCozinhaIT.java
@@ -2,9 +2,11 @@ package com.willardy.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,11 +24,16 @@ class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate(); //inlcuido para resetar o banco de dados a cada execução
     }
 
     @Test
@@ -49,5 +56,17 @@ class CadastroCozinhaIT {
                 .body("", hasSize(4))
                 //Essa linha nao é necessario nesse teste, porem fica de exemplo para testes onde precisam validar campos dos objetos de retorno
                 .body("nome", hasItems("Tailandesa", "Brasileira"));
+    }
+
+    @Test
+    public void testRetornarStatus201_QuandoCadastrarCozinha() {
+        given()
+                .body("{ \"nome\": \"Francesa\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 }
